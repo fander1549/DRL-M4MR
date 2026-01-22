b889c8d3ea92e690a2c4ebdfde8903bb8a562073
rfande <2665314410a@gmail.com>
2026-01-22 17:11:27 +0800

    3

--- mininet/generate_matrices.py
+++ mininet/generate_matrices.py
@@ -19,8 +19,10 @@ def set_seed():
 def generate_tm():
     num_nodes = int(args.num_nodes)
     num_tms = int(args.num_tms)
+    diurnal_freq = int(args.diurnal_freq) if args.diurnal_freq >= 1 else int(round(1 / args.diurnal_freq))
+
     tm = modulated_gravity_tm(num_nodes, num_tms, int(args.mean_traffic), args.pm_ratio, args.t_ratio,
-                              args.diurnal_freq, int(args.spatial_variance), args.temporal_variance)
+                              diurnal_freq, int(args.spatial_variance), args.temporal_variance)
 
     mean_time_tm = []
     for t in range(args.num_tms):
@@ -78,7 +80,7 @@ if __name__ == '__main__':
     parser.add_argument("--mean_traffic", type=int, default=5 * 10 ** 3 * 0.75, help="mean volume of traffic (Kbps)")
     parser.add_argument("--pm_ratio", default=1.5, help="peak-to-mean ratio")
     parser.add_argument("--t_ratio", default=0.75, help="trough-to-mean ratio")
-    parser.add_argument("--diurnal_freq", default=1 / 24, help="Frequency of modulation")
+    parser.add_argument("--diurnal_freq", default= 24, help="Frequency of modulation")
     parser.add_argument("--spatial_variance", type=int, default=500,
                         help="Variance on the volume of traffic between origin-destination pairs")
     parser.add_argument("--temporal_variance", default=0.03, help="Variance on the volume in time")
